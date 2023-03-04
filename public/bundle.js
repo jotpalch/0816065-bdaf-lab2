4b73f42695d3d5e95758005014b651bb2ea5e652
jotpalch <jotp076315217@gmail.com>
2023-03-05 05:37:30 +0800

    renew bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -5,10 +5,10 @@ const HDWallet = require('ethereum-hdwallet')
 function Generator(_pre = "aa") {
     // var _pre = "aa"
     var _pre_len = _pre.length
-
+    var r = ""
     while(1){
         var _mnemonic = new bip39();
-        var mnemonic =  _mnemonic.toString() ;
+        mnemonic =  _mnemonic.toString() ;
         // var mnemonic = bip39.generateMnemonic()
         // var seed = bip39.mnemonicToSeed(mnemonic);
         // var hdWallet = HDWallet.fromMasterSeed(seed);
@@ -20,17 +20,16 @@ function Generator(_pre = "aa") {
         if(address.substring(0, _pre_len) === _pre){
             var publick = hdwallet.derive(`m/44'/60'/0'/0/0`).getPublicKey().toString('hex')
             var privatek = hdwallet.derive(`m/44'/60'/0'/0/0`).getPrivateKey().toString('hex')
-            console.log("PublicKey: "+publick)
-            console.log("PrivateKey: "+privatek)
-            console.log(mnemonic)
-            return mnemonic;
+            // console.log("PublicKey: "+publick)
+            // console.log("PrivateKey: "+privatek)
+            // console.log(mnemonic)
+            return { publick: publick, privatek: privatek, mnemonic: mnemonic };
         }
-        // document.querySelector("#mnemonic").innerHTML = mnemonic;
     }
 }
 
-// Generator();
-console.log(Generator());
+// console.log(Generator());
+
 // document.querySelector("#mnemonic").innerHTML = "mnemonic";
 
 // export default Generator;
